7487530f4cff6d51082da8cd5459cd5c71957384
Ege Doğukan Seven <eseven20@ku.edu.tr>
2023-04-26 15:13:03 +0300

    feat: implemented clicking and responsing accordingly to territory tiles, fixed controller-territory bugs

--- src/RiskPackage/Territory.java
+++ src/RiskPackage/Territory.java
@@ -1,19 +1,14 @@
 package RiskPackage;
 
-import java.awt.Polygon;
-
-import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.*;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
-
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Territory extends JPanel implements MouseListener {
+public class Territory extends JPanel {
 	 private String name;
 	    private int xCoordinate;
 	    private int yCoordinate;
@@ -21,126 +16,96 @@ public class Territory extends JPanel implements MouseListener {
 	    private int height;
 	    private Color color;
 	    private ArrayList<Territory> neighbors;
-	    //private Army army;
-
-	 
-
 	    
 	    public Territory(int xCoordinate, int yCoordinate,int width,int height, String name, Color color) {
-	        this.setLayout(null);
 	        this.xCoordinate = xCoordinate;
 	        this.yCoordinate = yCoordinate;
 	        this.width = width;
 	        this.height = height;
-	        this.setBounds(xCoordinate, yCoordinate, width, height);
-	        
+			this.setSize(width, height);
+			this.setLocation(xCoordinate, yCoordinate);
+	        this.setBackground(Color.BLACK);
 	        this.setColor(color);
 	        this.name = name;
 	        this.color = color;
 	        this.neighbors = new ArrayList<Territory>();
 	        this.setOpaque(false);
-	        addMouseListener(this);
 	        this.setName(name);
-	        	
-	        
+			this.addMouseListener(new MouseAdapter() {
+				private Color oldColor;
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					
+					JOptionPane.showMessageDialog(null, "successfully clicked");
+					
+				}
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+					oldColor = getColor();
+					setColor(Color.RED);
+					repaint();
+				}
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					setColor(oldColor);
+					repaint();
+				}
+			});        
+	    }
+		
+		@Override
+	    public void paintComponent(Graphics g) {
+	    	super.paintComponent(g);
+	        g.setColor(this.getColor());
+	        g.fillRect(this.xCoordinate, this.yCoordinate, this.width, this.height);
+	        g.setColor(Color.BLACK);
+	        g.drawString(this.name, this.xCoordinate, this.yCoordinate); 
 	    }
 	    
-	    
-	    
-	    
 	    public int getWidth() {
-			return width;
+			return this.width;
 		}
-
-
-
 
 		public void setWidth(int width) {
 			this.width = width;
 		}
 
-
-
-
 		public int getHeight() {
-			return height;
+			return this.height;
 		}
-
-
-
 
 		public void setHeight(int height) {
 			this.height = height;
 		}
 
-
-
-
-		@Override
-	    public void paintComponent(Graphics g) {
-	    	super.paintComponent(g);
-
-	       
-	            // Draw the territory
-	            g.setColor(this.getColor());
-	            g.fillOval(this.getXCoordinate(), this.getYCoordinate(), this.getWidth(), this.getHeight());
-
-	            // Draw the territory name
-	            g.setColor(Color.BLACK);
-	            g.drawString(this.getName(), this.getBounds().x + this.getBounds().width / 2, this.getBounds().y + this.getBounds().height / 2);
-	            
-	            
-	            
-	        
-	    }
-
 		public int getxCoordinate() {
-			return xCoordinate;
+			return this.xCoordinate;
 		}
-
-
-
 
 		public void setxCoordinate(int xCoordinate) {
 			this.xCoordinate = xCoordinate;
 		}
 
-
-
-
 		public int getyCoordinate() {
-			return yCoordinate;
+			return this.yCoordinate;
 		}
-
-
-
 
 		public void setyCoordinate(int yCoordinate) {
 			this.yCoordinate = yCoordinate;
 		}
 
-
-
-
-
 	    public String getName() {
-	        return name;
+	        return this.name;
 	    }
-		
-	    public int getXCoordinate() {
-	        return xCoordinate;
-	    }
-
-	    public int getYCoordinate() {
-	        return yCoordinate;
-	    }
-	   
+		   
 	    public ArrayList<Territory> getNeighbors() {
-	        return neighbors;
+	        return this.neighbors;
 	    }
 
 	    public void addNeighbor(Territory neighbor) {
-	        neighbors.add(neighbor);
+	        this.neighbors.add(neighbor);
 	    }
 
 		public boolean contains(int x, int y) {
@@ -148,66 +113,10 @@ public class Territory extends JPanel implements MouseListener {
 		}
 		
 		public Color getColor() {
-			
-			return color;
+			return this.color;
 		}
+
 		public Color setColor(Color color) {
 			return this.color = color;
 		}
-		// public Army getArmies() {
-		  //      return this.army;
-		    //}
-
-		 //public void setArmies(Army army) {
-		//	 this.army = army;
-		  //  }
-
-
-
-
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-
-
-
-		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-
-
-
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-
-
-
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-
-
-
-		@Override
-		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		
-
-
 }
