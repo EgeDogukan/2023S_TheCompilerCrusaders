5dfe06ca445b936f34926801d4f67dd3ac4f21aa
begumayten <79323462+begumayten@users.noreply.github.com>
2023-05-03 22:07:00 +0300

    map updated
    continents and territories added

--- src/uipackage/BuildingMode.java
+++ src/uipackage/BuildingMode.java
@@ -56,42 +56,153 @@ public class BuildingMode extends JFrame {
 
         ArrayList<Territory> europeTerritories = new ArrayList<>();
 
-        ArrayList<Territory> americaTerritories = new ArrayList<>();
-
-        Continents Africa = new Continents("Africa", africaTerritories, 300, 300, Color.yellow);
-        Africa.setLocation(900, 50);
-
-        Continents Europe = new Continents("Europe",europeTerritories, 300, 300, Color.RED);
-        Europe.setLocation(300, 280);
-
-        Continents America = new Continents("America",americaTerritories, 200, 200, Color.magenta);
-        America.setLocation(150, 50);
+        ArrayList<Territory> northAmericaTerritories = new ArrayList<>();
         
-        Continents Asia = new Continents("Asia",asiaTerritories, 200, 200, Color.black);
-        Asia.setLocation(900, 450);
+        ArrayList<Territory> southAmericaTerritories = new ArrayList<>();
+        
+        ArrayList<Territory> australiaTerritories = new ArrayList<>();        
 
         
-        Territory Holland = new Territory(250, 250, 40, 40, "holland", Color.BLUE, Europe);
-        Territory US1 = new Territory(75, 150, 40, 40, "US", Color.BLUE, America);
-        Territory Canada = new Territory(230, 250, 40, 40, "Canada", Color.BLUE, America);
-        Territory UK = new Territory(250, 150, 40, 40, "UK", Color.BLUE, Europe);
-        Territory India = new Territory(280, 300, 40, 40, "India", Color.BLUE, Asia);
-        Territory China = new Territory(250, 100, 40, 40, "China", Color.BLUE, Asia);
+        Continents Africa = new Continents("Africa", africaTerritories, 200, 300, Color.LIGHT_GRAY);
+        Africa.setLocation(750, 300);
+
+        Continents Europe = new Continents("Europe",europeTerritories, 300, 200, Color.LIGHT_GRAY);
+        Europe.setLocation(700, 70);
+
+        Continents NorthAmerica = new Continents("North America",northAmericaTerritories, 400, 300, Color.LIGHT_GRAY);
+        NorthAmerica.setLocation(150, 50);
+        
+        Continents SouthAmerica = new Continents("South America",southAmericaTerritories, 200, 200, Color.LIGHT_GRAY);
+        SouthAmerica.setLocation(200, 400);
+        
+        Continents Asia = new Continents("Asia",asiaTerritories, 350, 300, Color.LIGHT_GRAY);
+        Asia.setLocation(1050, 65);
+        
+        Continents Australia = new Continents("Australia",australiaTerritories, 200, 200, Color.LIGHT_GRAY);
+        Australia.setLocation(1150, 450);
+        
+        
+        Territory Alaska = new Territory(50, 50, 50, 50, "Alaska", Color.BLUE, NorthAmerica);
+        Territory Alberta = new Territory(150, 50, 50, 50, "Alberta", Color.BLUE, NorthAmerica);
+        Territory NorthWestTerritory = new Territory(250, 50, 50, 50, "NorthWestTerritory", Color.BLUE, NorthAmerica);
+        Territory Ontario = new Territory(350, 50, 50, 50, "Ontario", Color.BLUE, NorthAmerica);
+        Territory Quebec = new Territory(450, 50, 50, 50, "Quebec", Color.BLUE, NorthAmerica);
+        Territory Greenland = new Territory(50, 150, 50, 50, "Greenland", Color.BLUE, NorthAmerica);
+        Territory WesternUnitedStates = new Territory(50, 250, 50, 50, "WesternUnitedStates", Color.BLUE, NorthAmerica);
+        Territory EasternUnitedStates = new Territory(50, 350, 50, 50, "EasternUnitedStates", Color.BLUE, NorthAmerica);
+        Territory CentralAmerica = new Territory(50, 450, 50, 50, "CentralAmerica", Color.BLUE, NorthAmerica);
+        
+        Territory Venezuela = new Territory(50, 50, 50, 50, "Venezuela", Color.BLUE, SouthAmerica);
+        Territory Brazil = new Territory(150, 50, 50, 50, "Brazil", Color.BLUE, SouthAmerica);
+        Territory Peru = new Territory(150, 150, 50, 50, "Peru", Color.BLUE, SouthAmerica);
+        Territory Argentina = new Territory(250, 150, 50, 50, "Argentina", Color.BLUE, SouthAmerica);
+        
+        Territory Iceland = new Territory(50, 50, 50, 50, "Iceland", Color.BLUE, Europe);
+        Territory GreatBritain  = new Territory(150, 50, 50, 50, "GreatBritain ", Color.BLUE, Europe);
+        Territory Scandinavia = new Territory(250, 50, 50, 50, "Scandinavia", Color.BLUE, Europe);
+        Territory Ukraine = new Territory(350, 50, 50, 50, "Ukraine", Color.BLUE, Europe);
+        Territory NorthernEurope = new Territory(450, 50, 50, 50, "NorthernEurope", Color.BLUE, Europe);
+        Territory WesternEurope = new Territory(150, 150, 50, 50, "WesternEurope", Color.BLUE, Europe);
+        Territory SouthernEurope = new Territory(150, 250, 50, 50, "SouthernEurope", Color.BLUE, Europe);
+
+        Territory NorthAfrica = new Territory(50, 50, 50, 50, "NorthAfrica", Color.BLUE, Africa);
+        Territory Egypt = new Territory(150, 50, 50, 50, "Egypt", Color.BLUE, Africa);
+        Territory EastAfrica = new Territory(250, 50, 50, 50, "EastAfrica", Color.BLUE, Africa);
+        Territory Congo = new Territory(350, 50, 50, 50, "Congo", Color.BLUE, Africa);
+        Territory SouthAfrica = new Territory(150, 150, 50, 50, "SouthAfrica", Color.BLUE, Africa);
+        Territory Madagascar = new Territory(250, 150, 50, 50, "Madagascar", Color.BLUE, Africa);
+        
+        Territory MiddleEast = new Territory(50, 50, 50, 50, "MiddleEast", Color.BLUE, Asia);
+        Territory Afghanistan = new Territory(150, 50, 50, 50, "Afghanistan", Color.BLUE, Asia);
+        Territory Ural = new Territory(250, 50, 50, 50, "Ural", Color.BLUE, Asia);
+        Territory India = new Territory(350, 50, 50, 50, "India", Color.BLUE, Asia);
+        Territory Siberia = new Territory(450, 50, 50, 50, "Siberia", Color.BLUE, Asia);
+        Territory Yakutsk = new Territory(550, 50, 50, 50, "Yakutsk", Color.BLUE, Asia);
+        Territory Irkutsk = new Territory(150, 150, 50, 50, "Irkutsk", Color.BLUE, Asia);
+        Territory Kamchatka = new Territory(150, 250, 50, 50, "Kamchatka", Color.BLUE, Asia);
+        Territory Mongolia = new Territory(150, 350, 50, 50, "Mongolia", Color.BLUE, Asia);
+        Territory China = new Territory(150, 450, 50, 50, "China", Color.BLUE, Asia);
+        Territory Siam = new Territory(150, 550, 50, 50, "Siam", Color.BLUE, Asia);
+        Territory Japan = new Territory(250, 250, 50, 50, "Japan", Color.BLUE, Asia);
+        
+        Territory Indonesia = new Territory(50, 50, 50, 50, "Indonesia", Color.BLUE, Australia);
+        Territory NewGuinea = new Territory(150, 50, 50, 50, "NewGuinea", Color.BLUE, Australia);
+        Territory WesternAustralia = new Territory(150, 150, 50, 50, "WesternAustralia", Color.BLUE, Australia);
+        Territory EasternAustralia = new Territory(250, 150, 50, 50, "EasternAustralia", Color.BLUE, Australia);
+        
+
+        //Territory Holland = new Territory(250, 250, 40, 40, "holland", Color.BLUE, Europe);
+        //Territory US1 = new Territory(75, 150, 40, 40, "US", Color.BLUE, NorthAmerica);
+        //Territory Canada = new Territory(230, 250, 40, 40, "Canada", Color.BLUE, NorthAmerica);
+        //Territory UK = new Territory(250, 150, 40, 40, "UK", Color.BLUE, Europe);
+        //Territory India = new Territory(280, 300, 40, 40, "India", Color.BLUE, Asia);
+        //Territory China = new Territory(250, 100, 40, 40, "China", Color.BLUE, Asia);
 
         ArrayList<Territory> halilT = new ArrayList<>();
-        halilT.add(Holland);
-        halilT.add(UK);
-        halilT.add(China);
-        halilT.add(US1);
+        //halilT.add(Holland);
+        //halilT.add(UK);
+        //halilT.add(China);
+        //halilT.add(US1);
+        
+        
+        
+        NorthAmerica.addTerritory(Alaska);
+        NorthAmerica.addTerritory(Alberta);
+        NorthAmerica.addTerritory(NorthWestTerritory);
+        NorthAmerica.addTerritory(Ontario);
+        NorthAmerica.addTerritory(Quebec);
+        NorthAmerica.addTerritory(Greenland);
+        NorthAmerica.addTerritory(WesternUnitedStates);
+        NorthAmerica.addTerritory(EasternUnitedStates);
+        NorthAmerica.addTerritory(CentralAmerica);
+        
+        SouthAmerica.addTerritory(Venezuela);
+        SouthAmerica.addTerritory(Peru);
+        SouthAmerica.addTerritory(Brazil);
+        SouthAmerica.addTerritory(Argentina);
+        
+        Europe.addTerritory(Iceland);
+        Europe.addTerritory(GreatBritain );
+        Europe.addTerritory(Scandinavia);
+        Europe.addTerritory(Ukraine);
+        Europe.addTerritory(NorthernEurope);
+        Europe.addTerritory(SouthernEurope);
+        Europe.addTerritory(WesternEurope);
+        
+        Africa.addTerritory(NorthAfrica);
+        Africa.addTerritory(Egypt);
+        Africa.addTerritory(Madagascar);
+        Africa.addTerritory(SouthAfrica);
+        Africa.addTerritory(EastAfrica);
+        Africa.addTerritory(Congo);
+        
+        Asia.addTerritory(Japan);
+        Asia.addTerritory(Siam);
         Asia.addTerritory(China);
-        Africa.addTerritory(Canada);
-        America.addTerritory(US1);
-        Europe.addTerritory(Holland);
-        Europe.addTerritory(UK);
-        Africa.addTerritory(India);
+        Asia.addTerritory(Mongolia);
+        Asia.addTerritory(Kamchatka);
+        Asia.addTerritory(Irkutsk);
+        Asia.addTerritory(Yakutsk);
+        Asia.addTerritory(Siberia);
+        Asia.addTerritory(India);
+        Asia.addTerritory(Ural);
+        Asia.addTerritory(Afghanistan);
+        Asia.addTerritory(MiddleEast);
+        
+        Australia.addTerritory(Indonesia);
+        Australia.addTerritory(NewGuinea);
+        Australia.addTerritory(EasternAustralia);
+        Australia.addTerritory(WesternAustralia);
+        
+//        Asia.addTerritory(China);
+//        Africa.addTerritory(Canada);        
+//        Europe.addTerritory(Holland);
+//        Europe.addTerritory(UK);
+//        Africa.addTerritory(India);
+        
         ArrayList<Territory> compT = new ArrayList<>();
-        compT.add(Canada);
-        compT.add(India);
+        //compT.add(Canada);
+        //compT.add(India);
 
 //        HumanPlayer halil= new HumanPlayer(899, Color.PINK, halilT);
 //        ComputerPlayer comp1 = new ComputerPlayer(1, Color.green, compT);
@@ -102,8 +213,10 @@ public class BuildingMode extends JFrame {
 
         this.add(Europe);
         this.add(Asia);
-        this.add(America);
+        this.add(NorthAmerica);
+        this.add(SouthAmerica);
         this.add(Africa);
+        this.add(Australia);
         this.pack();
         
         
@@ -112,40 +225,41 @@ public class BuildingMode extends JFrame {
         
 
         continents.add(Asia);
-        continents.add(America);
+        continents.add(NorthAmerica);
+        continents.add(SouthAmerica);
         continents.add(Europe);
         continents.add(Africa);
-        
+        continents.add(Australia);
         
         
         String[] numberOfPlayers = {"2", "3", "4", "5", "6"};
         JComboBox<String> myComboBox = new JComboBox<String>(numberOfPlayers);
         myComboBox.setName("numberOfHuman");
-        myComboBox.setSize(100,100);
+        myComboBox.setSize(100,50);
         myComboBox.setBackground(Color.WHITE);
-        myComboBox.setLocation(550, 650);
+        myComboBox.setLocation(550, 750);
         JLabel label = new JLabel("Number of Players:");
         label.setLabelFor(myComboBox);
         this.getContentPane().add(myComboBox);
         this.getContentPane().add(label);
-        label.setBounds(550,580,300,100);
+        label.setBounds(550,680,300,100);
 
         String[] numberOfComp = {"2","1"};
         JComboBox<String> compBox = new JComboBox<String>(numberOfComp);
         compBox.setName("numberOfComp");
-        compBox.setSize(100,100);
+        compBox.setSize(100,50);
         compBox.setBackground(Color.WHITE);
-        compBox.setLocation(250, 650);
+        compBox.setLocation(250, 750);
         JLabel CompL = new JLabel("Number of AI's:");
         CompL.setLabelFor(myComboBox);
         this.getContentPane().add(compBox);
         this.getContentPane().add(CompL);
-        CompL.setBounds(250,580,300,100);
+        CompL.setBounds(250,680,300,100);
 
         JButton startButton = new JButton("Start Game!");
-        startButton.setSize(100,100);
+        startButton.setSize(150,50);
         startButton.setBackground(Color.GRAY);
-        startButton.setLocation(950, 650);
+        startButton.setLocation(950, 750);
         this.getContentPane().add(startButton);
         
         startButton.addMouseListener(new MouseAdapter() {
