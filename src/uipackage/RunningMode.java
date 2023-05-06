6f2a87f3cd7328f02104645435c14d2e7a103b6f
keremiis <kbasol19@ku.edu.tr>
2023-05-06 16:24:50 +0300

    Update RunningMode.java

--- src/uipackage/RunningMode.java
+++ src/uipackage/RunningMode.java
@@ -73,7 +73,7 @@ public class RunningMode extends JFrame{
             		RunningMode.turnCounter++;
             	}
             	turn.setText(Integer.toString(RunningMode.turnCounter));
-
+    
             }
         });
 
