d3c5b16f151d9ae4cd6029999c698c14f9c98e64
Ege Doğukan Seven <eseven20@ku.edu.tr>
2023-05-06 12:50:56 +0300

    turn obj init

---
+++ src/RiskPackage/Turn.java
@@ -0,0 +1,14 @@
+package RiskPackage;
+
+public class Turn extends Thread {
+
+    int currentTurn = 0;
+    int nextTurn = 0;
+
+    public void Turn(int playerNumber, int playerType) {
+        
+    }
+    public void run() {
+
+    }
+}
