c2571bdd93460ea4ef53242b155b30e7c8dc5f8a
Ege Doğukan Seven <eseven20@ku.edu.tr>
2023-05-24 21:25:36 +0300

    second test added

--- src/worldmaptests/worldmaptests.java
+++ src/worldmaptests/worldmaptests.java
@@ -15,14 +15,24 @@ public class worldmaptests {
     different regions and provides functionality to identify and highlight the clicked regions. The class uses Java's AWT and Swing libraries to handle graphics 
     and user input. The map image is loaded from a file and processed to extractregions and shapes. The class provides methods to manipulate the map, 
     such as changing shape colors and retrieving information about clicked shapes.*/
-	
+
+    /*Abstract Function:
+    The WorldMap class represents a graphical user interface component that displays a world map and allows users to interact with it. It provides methods to 
+    handle user input, extract regions and shapes from the map image, and manipulate the map's appearance.*/
 	
 	@Test
-    public void testShapeListInit() {
+    public void testShapeListInit() {                   // testing if shapelist init correctly
         WorldMap worldMap = new WorldMap();
         ArrayList<Shape> shapeList = worldMap.getShapeList();
         Assertions.assertFalse(shapeList.isEmpty(), "Shape list should not be empty");
-        Assertions.assertEquals(103, shapeList.size(), "Expected number of shapes: 12");
+        Assertions.assertEquals(103, shapeList.size(), "Expected number of shapes: 103");
+    }
+
+    @Test
+    public void testClickedShapeNullInitially() {       //testing clicked shape init correctly and working
+        WorldMap worldMap = new WorldMap();
+        Shape clickedShape = worldMap.getClickedShape();
+        Assertions.assertNull(clickedShape, "Clicked shape should be null initially");
     }
     
 }
