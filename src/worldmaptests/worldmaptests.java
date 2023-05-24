9c20b211d49521827bde7c39335fb358ccbe9a65
EgeDogukan <eseven20@ku.edu.tr>
2023-05-24 16:38:01 +0300

    overview

--- src/worldmaptests/worldmaptests.java
+++ src/worldmaptests/worldmaptests.java
@@ -2,4 +2,11 @@ package worldmaptests;
 
 public class worldmaptests {
     //world map class tests
+
+    //Overview of WorldMap: 
+    /*The WorldMap class represents a graphical user interface component that displays a world map. It allows users to interact with the map by clicking on 
+    different regions and provides functionality to identify and highlight the clicked regions. The class uses Java's AWT and Swing libraries to handle graphics 
+    and user input. The map image is loaded from a file and processed to extractregions and shapes. The class provides methods to manipulate the map, 
+    such as changing shape colors and retrieving information about clicked shapes.*/
+    
 }
