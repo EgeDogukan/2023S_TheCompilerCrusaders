24e9a0ecde957efb2d063cf4961d0c0ded3a0fa6
Ege Doğukan Seven <eseven20@ku.edu.tr>
2023-05-24 21:19:31 +0300

    first test, test if the shape list init correctly

--- src/worldmaptests/worldmaptests.java
+++ src/worldmaptests/worldmaptests.java
@@ -1,5 +1,12 @@
 package worldmaptests;
 
+import java.awt.Shape;
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import uipackage.WorldMap;
+
 public class worldmaptests {
     //world map class tests
 
@@ -8,5 +15,14 @@ public class worldmaptests {
     different regions and provides functionality to identify and highlight the clicked regions. The class uses Java's AWT and Swing libraries to handle graphics 
     and user input. The map image is loaded from a file and processed to extractregions and shapes. The class provides methods to manipulate the map, 
     such as changing shape colors and retrieving information about clicked shapes.*/
+	
+	
+	@Test
+    public void testShapeListNotEmpty() {
+        WorldMap worldMap = new WorldMap();
+        ArrayList<Shape> shapeList = worldMap.getShapeList();
+        Assertions.assertFalse(shapeList.isEmpty(), "Shape list should not be empty");
+        Assertions.assertEquals(103, shapeList.size(), "Expected number of shapes: 12");
+    }
     
 }
