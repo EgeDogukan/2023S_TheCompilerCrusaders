71d387da09e198462b5c4807e2838ac722a9ba7a
Ege Doğukan Seven <eseven20@ku.edu.tr>
2023-06-02 00:06:40 +0300

    test specifications added

--- src/worldmaptests/worldmaptests.java
+++ src/worldmaptests/worldmaptests.java
@@ -20,6 +20,12 @@ public class worldmaptests {
     The WorldMap class represents a graphical user interface component that displays a world map and allows users to interact with it. It provides methods to 
     handle user input, extract regions and shapes from the map image, and manipulate the map's appearance.*/
 	
+
+        // Test case for verifying the initialization of the shape list in WorldMap class.
+        // Requires: None.
+        // Modifies: None.
+        // Effects: - Verifies that the shape list is not empty.
+        //          - Verifies that the expected number of shapes (103) is initialized in the shape list.
 	@Test
     public void testShapeListInit() {                   // testing if shapelist init correctly
         WorldMap worldMap = new WorldMap();
@@ -28,6 +34,10 @@ public class worldmaptests {
         Assertions.assertEquals(103, shapeList.size(), "Expected number of shapes: 103");
     }
 
+        // Test case for verifying the initial state of the clicked shape in WorldMap class.
+        // Requires: None.
+        // Modifies: None.
+        // Effects: - Verifies that the clicked shape is null initially.
     @Test
     public void testClickedShapeNullInitially() {       //testing clicked shape init correctly and working
         WorldMap worldMap = new WorldMap();
@@ -35,6 +45,10 @@ public class worldmaptests {
         Assertions.assertNull(clickedShape, "Clicked shape should be null initially");
     }
 
+        // Test case for setting the color of a shape in WorldMap class to blue.
+        // Requires: None.
+        // Modifies: The color of the specified shape in the color list.
+        // Effects: - Verifies that the color of the shape is changed to blue.
     @Test
     public void testSetShapeColorToBlue() {
         WorldMap worldMap = new WorldMap();
@@ -48,6 +62,10 @@ public class worldmaptests {
         Assertions.assertEquals(Color.BLUE, colorList.get(0), "Shape color should be changed to blue");
     }
 
+        // Test case for setting the color of a shape in WorldMap class to red.
+        // Requires: None.
+        // Modifies: The color of the specified shape in the color list.
+        // Effects: - Verifies that the color of the shape is changed to red.
     @Test
     public void testSetShapeColorToRed() {
         WorldMap worldMap = new WorldMap();
@@ -61,6 +79,10 @@ public class worldmaptests {
         Assertions.assertEquals(Color.RED, colorList.get(0), "Shape color should be changed to red");
     }
 
+        // Test case for resetting the color of a shape in WorldMap class.
+        // Requires: None.
+        // Modifies: The color of the specified shape in the color list.
+        // Effects: - Verifies that the color of the shape is reset to its original color.
     @Test
     public void testSetShapeColorReset() {
         WorldMap worldMap = new WorldMap();
@@ -79,12 +101,22 @@ public class worldmaptests {
         Assertions.assertEquals(originalColor, colorList.get(0), "Shape color should be reset");
     }
 
+        // Test case for the repOk() method of the WorldMap class.
+        // Requires: None.
+        // Modifies: None.
+        // Effects: - Verifies that the repOk() method returns true when the class invariant is satisfied.
     @Test
     public void testRepOk() {
         WorldMap worldMap = new WorldMap();
         Assertions.assertTrue(worldMap.repOk());
     }
 
+        // Test case for removing a shape from the shape list in WorldMap class.
+        // Requires: The specified shape exists in the shape list.
+        // Modifies: The shape list of WorldMap class.
+        // Effects: - Verifies that the specified shape is removed from the shape list.
+        //          - Verifies that the shape list no longer contains the removed shape after removal.
+        //          - Verifies that attempting to remove the same shape again has no effect on the shape list.
     @Test
     public void testRemoveFromShapeList() {
         WorldMap worldMap = new WorldMap();
